29e5215e5f29bc6a7f78f1630325a19cbd54e4b5
EunsuSeo01 <eunsu0613@naver.com>
2024-01-21 10:51:12 -0800

    회원가입 및 로그인 페이지의 배경화면 설정

---
+++ src/components/RegisterPage/Register.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Register.css'
+
+const Register = () => {
+  return (
+    <div className="register-container">
+      <div className="left-section">
+      </div>
+      <div className="right-section">
+      </div>
+    </div>
+  );
+};
+
+export default Register;
